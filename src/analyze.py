1419b63d5c369f91d98b9bf9716ce9a719d1d84f
Javier <info@marketingjdr.com>
2026-08-05 20:04:21 +0200

    Vamos por la mitad de sales_collection

--- src/analyze.py
+++ src/analyze.py
@@ -64,6 +64,7 @@ from sale import Sale
 from client_collection import ClientCollection
 from sales_collection import SalesCollection
 import json
+import csv
 import pandas as pd
 
 
@@ -73,16 +74,19 @@ what_to_do = int(input("Que es lo que quieres hacer (contesta con un número) 1:
 if(what_to_do == 1):
     what_to_do_client = int(input("1: Quieres buscar por un ID, 2: Quieres buscar por un pais "))
     def read_file():
-                with open("data/clients.json", "r", encoding="utf-8") as file:
-                    file.readline() # Leemos la primera fila para quitarnos el corchete
-                    for line in file: # Empezamos a leer todo el archivo línea por línea
-                        a_line = line.strip()
-                        if(a_line == "]"): # en caso de que llegue al corchete de cierre, terminamos el bucle ya que no nos sirve
-                            break
-                        if a_line.endswith(","): # Quitamos la coma al final para romperlo y tenerlo en diferentes piezas
-                            a_line = a_line[:-1]
-                        client = json.loads(a_line) # Transformamos de json a objeto --> Diccionario
-                        yield client # Enviamos el objeto a donde querramos, de uno en uno, para no saturar
+        try:
+            with open("data/clients.json", "r", encoding="utf-8") as file:
+                file.readline() # Leemos la primera fila para quitarnos el corchete
+                for line in file: # Empezamos a leer todo el archivo línea por línea
+                    a_line = line.strip()
+                    if(a_line == "]"): # en caso de que llegue al corchete de cierre, terminamos el bucle ya que no nos sirve
+                        break
+                    if a_line.endswith(","): # Quitamos la coma al final para romperlo y tenerlo en diferentes piezas
+                        a_line = a_line[:-1]
+                    client = json.loads(a_line) # Transformamos de json a objeto --> Diccionario
+                    yield client # Enviamos el objeto a donde querramos, de uno en uno, para no saturar
+        except Exception as e:
+            print(f"Ha ocurrido un error {e}")
 
     if(what_to_do_client == 1): # Si queremos buscar por el id
         search_client_id = int(input("Qué numero de cliente quieres buscar por id: (Debe de ser un número) "))
@@ -92,28 +96,59 @@ if(what_to_do == 1):
         print(get_client_id) # Imprimimos el resultado
 
 
-    elif(what_to_do_client == 2):
+    elif(what_to_do_client == 2): # Si queremos buscar por pais
         def all_country():
-            # Averiguar todos los paises que hay en el json
-            with open("data/clients.json", "r", encoding="utf-8") as file:
-                df = pd.read_json(file) #Utilizamos pandas porque es una opción mucho más rápida
-            all_country_file = list(df['country'].unique())
-            country_in_file = ""
-            for i in all_country_file:
-                country_in_file += f", {i}"
-            search_for_country = str(input(f"Los países que hay{country_in_file} escoje un pais ")).lower()
-            return search_for_country
+            
+            try: # Averiguar todos los paises que hay en el json, para ello debemos de preparar lo que tenemos que enviar a sales collection
+                with open("data/clients.json", "r", encoding="utf-8") as file:
+                    df = pd.read_json(file) #Utilizamos pandas porque es una opción mucho más rápida
+                all_country_file = list(df['country'].unique())
+                country_in_file = "" #Hay que saber todos los paises que son
+                for i in all_country_file:
+                    country_in_file += f", {i}"
+                search_for_country = str(input(f"Los países que hay{country_in_file} escoje un pais ")).lower()
+                return search_for_country
+
+            except Exception as e:
+                print(f"Ha ocurrido un error {e}")
 
         all_country_client = all_country()
-        client = read_file()
-        client_collections = ClientCollection(client)
+        client_country = read_file()
+        client_collections = ClientCollection(client_country)
         get_client_country = client_collections.clients_by_country(all_country_client)
         print(get_client_country)
 
 
+# * Ver toda la parte de ventas
 
 elif(what_to_do == 2):
-    pass
+
+    what_to_do_sales = int(input("Contesta con un número: 1: Quieres saber la cantidad de compra por ID, 2: Saber el total de dinero que han comprado por ID, 3: Saber el total de dinero obtenido por categorías "))
+    try:
+        def read_file():
+            with open("data/sales.csv", "r", newline="", encoding="utf-8") as file:
+                read_files = csv.reader(file) 
+                header = next(read_files)
+                for i in read_files:
+                    yield i
+        save_data = read_file()
+        all_object_class = SalesCollection(save_data)
+    except Exception as e:
+        print(f"Ha ocurrido un error {e}")
+
+
+    if(what_to_do_sales == 1): #  Saber la cantidad de compra por ID
+        result = all_object_class.sales_by_client()
+        print(result)
+
+    elif(what_to_do_sales == 2): # Saber el total de dinero que han comprado por ID
+        result = all_object_class.total_amount_by_client()
+        print(result)
+
+    elif(what_to_do_sales == 3):
+        result = all_object_class.total_amount_by_category()
+        print(result)
+
 
 else:
     print("No existe ese comando")
--- src/sales_collection.py
+++ src/sales_collection.py
@@ -5,22 +5,49 @@ from sale import Sale
 # ! Manejar toda la logica de las ventas
 
 class SalesCollection:
-    def __init__(self):
-        pass
+    def __init__(self, a_line_file):
+        self.a_line_file = a_line_file
 
-    def sales_by_client(client_id):
-        # Todas las ventas de un cliente
-        pass
+    def sales_by_client(self):
+        sales_client = {} # creamos un diccionario para poder guardar cada venta que ha realizado cada cliente
+        for i in self.a_line_file: # Dado que tenemos en el otro archivo un yield, necesitamos que lo vaya recorriendo poco a poco
+            client_id = i[1] # i[1] es el lugar donde se encuentran los ID
+            if client_id not in sales_client: # Hay que dividir entre los ID que tenemos y los que no tenemos, en caso de que client_id no se encuentre en sales_client, se crea uno nuevo con un valor de 1, en caso contrario, se le suma 1
+                sales_client[client_id] = 1
+            else:
+                sales_client[client_id] += 1
 
-    def total_amount_by_client(client_id):
+        return f"Los clientes han tenido estas Ventas: {sales_client}"
+
+
+
+    def total_amount_by_client(self):
         # Suma de importes de un cliente
-        pass
+        sales_amount_client = {} 
+        for i in self.a_line_file:
+            client_id = i[1]
+            amomunt_id = round(float(i[4]),2)
+            if client_id not in sales_amount_client:
+                sales_amount_client[client_id] = amomunt_id
+            else:
+                sales_amount_client[client_id] += amomunt_id
+        return sales_amount_client
 
-    def total_amount_by_category(category):
-        # Suma de ventas de una categoría
-        pass
+        
 
-    def average_sale_by_client(client_id):
+    def total_amount_by_category(self):
+        sales_amount_category = {} 
+        for i in self.a_line_file:
+            category = i[3]
+            amomunt = round(float(i[4]),2)
+            if category not in sales_amount_category:
+                sales_amount_category[category] = amomunt
+            else:
+                sales_amount_category[category] += amomunt
+        sales_amount_category = {value: round(key, 2) for value, key in sales_amount_category.items()}
+        return sales_amount_category
+
+    def average_sale_by_client(self, client_id):
         # Media de gasto por venta para un cliente
         pass
 
